1bf262cab115b052a726f42e7c6c27fbdad33446
irina <obesnyuk@gmail.com>
2023-09-27 02:13:05 +0300

    initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronimic;
+    public String phone;
+    public String lastName;
+    public boolean subscription;
+    public FormDate birthday;
+}
